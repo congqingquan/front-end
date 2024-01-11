81ab4cd8edae8bc77901429a8b850340b3f9c9c9
congqingquan <17600073620@163.com>
2024-01-11 18:00:56 +0800

    react - router

--- react-router-study/src/main.jsx
+++ react-router-study/src/main.jsx
@@ -3,28 +3,52 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'antd/dist/reset.css';
 
-import Root from './routes/root';
-import Contact from './routes/concat.jsx';
-import ErrorPage from './error-page.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
+import Contact, { loader as contactLoader } from './routes/concat.jsx';
+import ErrorPage from './error-page.jsx';
+import EditContact, { action as editAction } from './routes/edit.jsx';
+import { action as destroyAction } from './routes/destroy';
+import Index from './routes/index';
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: 'contacts/:contactId',
-                element: <Contact />,
-            },
-        ],
-    },
-    // {
-    //     path: 'contacts/:contactId',
-    //     element: <Contact />,
-    // },
-]);
+const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <Root />,
+            loader: rootLoader,
+            errorElement: <ErrorPage />,
+            action: rootAction,
+            children: [
+                {
+                    index: true,
+                    element: <Index />,
+                },
+                {
+                    path: 'contacts/:contactId',
+                    element: <Contact />,
+                    loader: contactLoader,
+                },
+                {
+                    path: 'contacts/:contactId/edit',
+                    element: <EditContact />,
+                    loader: contactLoader,
+                    action: editAction,
+                },
+                {
+                    path: 'contacts/:contactId/destroy',
+                    action: destroyAction,
+                    errorElement: <div>Oops! There was an error.</div>,
+                },
+            ],
+        },
+        // {
+        //     path: 'contacts/:contactId',
+        //     element: <Contact />,
+        // },
+    ],
+    {},
+);
 
 ReactDOM.createRoot(document.getElementById('root'), {
     identifierPrefix: 'react-id-',
--- react-router-study/src/routes/root.jsx
+++ react-router-study/src/routes/root.jsx
@@ -1,37 +1,97 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Form, NavLink, Link, Outlet, redirect, useLoaderData, useNavigation } from 'react-router-dom';
+import { createContact, getContacts } from '../contacts';
+
+export async function loader({ request }) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get('q');
+    const contacts = await getContacts(q);
+    return { contacts };
+}
+
+export async function action() {
+    const contact = await createContact();
+    return redirect(`/contacts/${contact.id}/edit`);
+}
 
 export default function Root() {
+    const { contacts } = useLoaderData();
+    const navigation = useNavigation();
     return (
         <>
             <div id="sidebar">
                 <h1>React Router Contacts</h1>
                 <div>
-                    <form id="search-form" role="search">
+                    {/*<form id="search-form" role="search">*/}
+                    {/*    <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" />*/}
+                    {/*    <div id="search-spinner" aria-hidden hidden={true} />*/}
+                    {/*    <div className="sr-only" aria-live="polite"></div>*/}
+                    {/*</form>*/}
+                    <Form id="search-form" role="search">
                         <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" />
                         <div id="search-spinner" aria-hidden hidden={true} />
                         <div className="sr-only" aria-live="polite"></div>
-                    </form>
-                    <form method="post">
+                    </Form>
+
+                    {/*<form method="post">*/}
+                    {/*    <button type="submit">New</button>*/}
+                    {/*</form>*/}
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
-                    <ul>
-                        <li>
-                            {/*<a href={`/contacts/1`}>Your Name</a>*/}
-                            {/* immediately render new UI, You can open the network tab in the browser devtools to see that it's not requesting documents anymore. */}
-                            <Link to={`contacts/1`}>Your Name</Link>
-                        </li>
-                        <li>
-                            {/* the browser is doing a full document request  */}
-                            <a href={`/contacts/2`}>Your Friend</a>
-                            {/*<Link to={`contacts/1`}>Your Name</Link>*/}
-                        </li>
-                    </ul>
+                    {/*<ul>*/}
+                    {/*    <li>*/}
+                    {/*        /!*<a href={`/contacts/1`}>Your Name</a>*!/*/}
+                    {/*        /!* immediately render new UI, You can open the network tab in the browser devtools to see that it's not requesting documents anymore. *!/*/}
+                    {/*        <Link to={`contacts/1`}>Your Name</Link>*/}
+                    {/*    </li>*/}
+                    {/*    <li>*/}
+                    {/*        /!* the browser is doing a full document request  *!/*/}
+                    {/*        <a href={`contacts/2`}>Your Friend</a>*/}
+                    {/*        /!*<Link to={`contacts/1`}>Your Name</Link>*!/*/}
+                    {/*    </li>*/}
+                    {/*</ul>*/}
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    {/*<Link to={`contacts/${contact.id}`}>*/}
+                                    {/*    {contact.first || contact.last ? (*/}
+                                    {/*        <>*/}
+                                    {/*            {contact.first} {contact.last}*/}
+                                    {/*        </>*/}
+                                    {/*    ) : (*/}
+                                    {/*        <i>No Name</i>*/}
+                                    {/*    )}{' '}*/}
+                                    {/*    {contact.favorite && <span>★</span>}*/}
+                                    {/*</Link>*/}
+                                    <NavLink
+                                        to={`contacts/${contact.id}`}
+                                        className={({ isActive, isPending }) =>
+                                            isActive ? 'active' : isPending ? 'pending' : ''
+                                        }>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{' '}
+                                        {contact.favorite && <span>★</span>}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                    )}
                 </nav>
             </div>
             <Outlet>
-                <div id="detail"></div>
+                <div id="detail" className={navigation.state === 'loading' ? 'loading' : ''}></div>
             </Outlet>
         </>
     );
